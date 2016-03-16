a4e4a5a56c18902dd2d0155663ed19a7e95f1232
Stefan <andos1@bfh.ch>
2016-03-16 11:06:49 +0100

    block deleter

---
+++ src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/BlockDeleter.java
@@ -0,0 +1,130 @@
+package ch.bfh.blk2.bitcoin.blockchain2database;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BlockDeleter {
+	
+	
+	private static final Logger logger = LogManager.getLogger("BlockDeleter");
+	
+	String GET_BLOCK_ID = "SELECT blk_id FROM block WHERE hash = ?",
+		GET_TRANSACTION_ID = "SELECT tx_id FROM transaction WHERE blk_id = ?",
+		GET_OUTPUT_ID = "SELECT output_id FROM output WHERE tx_id = ?",
+		GET_INPUT_ID = "SELECT input_id FROM input WHERE tx_id= ?",
+		
+		REMOVE_OUTPUT_SCRIPT = "DELETE FROM script WHERE output_id = ?",
+		
+		REMOVE_INPUT_SCRIPT = "DELETE FROM script WHERE input_id = ?",
+		
+		REMOVE_OUTPUT = "DELETE FORM output WHERE tx_id = ?",
+		
+		REMOVE_INPUT = "DELETE FROM input WHERE tx_id = ?",
+		
+		MARK_AS_UNSPENT="UPDATE output"
+				+ "SET spent=0,"
+				+ "spent_by_input=NULL,"
+				+ "spent_in_tx=NULL,"
+				+ "spent_at = NULL,"
+				+ "WHERE spent_in_tx = ?";
+	
+	
+	public void deleteBlock(String blockHash,DatabaseConnection connection){
+	
+		
+		try {
+			
+			//get block id
+			
+			long blkId=-1;
+			
+			PreparedStatement statement = connection.getPreparedStatement(GET_BLOCK_ID);
+			ResultSet result;
+			
+			statement.setString(1, blockHash);
+			result = statement.executeQuery();
+			if(result.next())
+				blkId = result.getLong(0);
+			result.close();
+			statement.close();
+		
+		if(blkId <0){
+			logger.trace("block not in database: "+blockHash );
+			return;
+		}
+		
+		//get transactions of this block
+		
+		ResultSet transactions=getFromDB(GET_TRANSACTION_ID, blkId, connection);
+		
+		for(int i = 0;transactions.next();i++){
+			
+			long txId =transactions.getLong(i);
+			
+			//get output and input ids and remove scripts
+			ResultSet outputs = getFromDB(GET_OUTPUT_ID, txId, connection);
+			for(int j = 0;outputs.next();j++){
+				long outputId = result.getLong(j);
+				removeFromDB(REMOVE_OUTPUT_SCRIPT, outputId, connection);
+			}
+			outputs.close();
+			
+			ResultSet inputs = getFromDB(GET_INPUT_ID, txId, connection);
+			for(int j = 0;inputs.next();j++){
+				long inputId = result.getLong(j);
+				removeFromDB(REMOVE_INPUT_SCRIPT, inputId, connection);
+			}
+			inputs.close();
+			
+			//remove outputs and inputs
+			removeFromDB(REMOVE_OUTPUT, txId, connection);
+			
+			//remove inputs
+			removeFromDB(REMOVE_INPUT, txId , connection);
+			
+			//set outputs to unspent
+			PreparedStatement unspentstatement = connection.getPreparedStatement(MARK_AS_UNSPENT);
+			unspentstatement.executeUpdate();
+			unspentstatement.close();
+			
+		}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private ResultSet getFromDB(String sql,long id, DatabaseConnection connection){
+		
+		PreparedStatement statement = connection.getPreparedStatement(sql);		
+		ResultSet result=null;
+		
+		try {
+			statement.setLong(1, id);
+			result = statement.executeQuery();	
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	private void removeFromDB(String sql,long id,DatabaseConnection connection){
+		PreparedStatement statement = connection.getPreparedStatement(sql);				
+		try {
+			statement.setLong(1, id);
+			statement.executeUpdate();	
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
