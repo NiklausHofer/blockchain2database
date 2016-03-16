a4c65902bba30c98e5126d262d33a54a30a1fd5e
Stefan <andos1@bfh.ch>
2016-03-16 17:28:40 +0100

    blockdeleter blockhash in whereclause + replaced false coloumn index with coloumn name

--- src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/BlockDeleter.java
+++ src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/BlockDeleter.java
@@ -5,92 +5,84 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class BlockDeleter {
 
+
 	private static final Logger logger = LogManager.getLogger("BlockDeleter");
 
-	String GET_BLOCK_ID = "SELECT blk_id FROM block WHERE hash = ?;",
+	String GET_BLOCK_ID = "SELECT blk_id FROM block WHERE hash = ?",
 			GET_TRANSACTION_ID = "SELECT tx_id FROM transaction WHERE blk_id = ?",
 			GET_OUTPUT_ID = "SELECT output_id FROM output WHERE tx_id = ?",
 			GET_INPUT_ID = "SELECT input_id FROM input WHERE tx_id= ?",
 
-	REMOVE_OUTPUT_SCRIPT = "DELETE FROM script WHERE output_id = ?",
+			REMOVE_OUTPUT_SCRIPT = "DELETE FROM script WHERE output_id = ?",
 			REMOVE_INPUT_SCRIPT = "DELETE FROM script WHERE input_id = ?",
-			REMOVE_OUTPUT = "DELETE FORM output WHERE tx_id = ?", REMOVE_INPUT = "DELETE FROM input WHERE tx_id = ?",
+			REMOVE_OUTPUT = "DELETE FORM output WHERE tx_id = ?",
+			REMOVE_INPUT = "DELETE FROM input WHERE tx_id = ?",
 			REMOVE_BLOCK = "DELETE FROM block WHERE blk_id = ?",
 			REMOVE_TRANSACTION = "DELETE FROM transaction WHERE tx_id = ?",
 
-	MARK_AS_UNSPENT = "UPDATE output"
-			+ "SET spent=0,"
-			+ "spent_by_input = NULL,"
-			+ "spent_in_tx = NULL,"
-			+ "spent_at = NULL,"
-			+ "WHERE spent_in_tx = ?";
+			MARK_AS_UNSPENT="UPDATE output"
+					+ "SET spent=0,"
+					+ "spent_by_input = NULL,"
+					+ "spent_in_tx = NULL,"
+					+ "spent_at = NULL,"
+					+ "WHERE spent_in_tx = ?";
 
-	private List<PreparedStatement> statements = new ArrayList<>();
+	private List<PreparedStatement> statements= new ArrayList<>();
 
-	public void deleteBlock(String blockHash, DatabaseConnection connection) {
-		logger.debug("deleteBlock() got called to delete Block " + blockHash);
+	public void deleteBlock(String blockHash,DatabaseConnection connection){
 
 		try {
 
 			//get block id
-			long blkId = -1;
-			ResultSet result = getFromDB(GET_BLOCK_ID, blkId, connection);
-			logger.debug(result.toString());
-
-			if (result.next())
+			long blkId=-1;
+			
+			PreparedStatement statement = connection.getPreparedStatement(GET_BLOCK_ID);
+			statement.setString(1,blockHash);
+			ResultSet result = statement.executeQuery();
+			
+			if(result.next())
 				blkId = result.getLong("blk_id");
-			else {
-				logger.fatal(
-						"Got an unexpected result from the database while looking for the id of Block " + blockHash);
-				connection.commit();
-				connection.closeConnection();
-				System.exit(1);
-			}
 			result.close();
+			statement.close();
 
-			if (blkId < 0) {
-				logger.fatal("Failed to delete Block " + blockHash + ". Unable to find it's ID in the DB");
+			if(blkId <0){
+				logger.fatal("block not in database: "+blockHash );
 				connection.commit();
 				connection.closeConnection();
 				System.exit(1);
 			}
-
-			logger.info("Identified blk_id " + blkId);
 
 			//get transactions of this block
 
-			ResultSet transactions = getFromDB(GET_TRANSACTION_ID, blkId, connection);
+			ResultSet transactions=getFromDB(GET_TRANSACTION_ID, blkId, connection);
 
-			for (int i = 1; transactions.next(); i++) {
+			while(transactions.next()){
 
-				long txId = transactions.getLong(i);
+				long txId =transactions.getLong("tx_id");
 
 				//get output and input ids and remove scripts
 				ResultSet outputs = getFromDB(GET_OUTPUT_ID, txId, connection);
-				for (int j = 1; outputs.next(); j++) {
-					logger.trace("Removing outputs");
-					long outputId = result.getLong(j);
+				while(outputs.next()){
+					long outputId = result.getLong("output_id");
 					removeFromDB(REMOVE_OUTPUT_SCRIPT, outputId, connection);
 				}
 				outputs.close();
 
 				ResultSet inputs = getFromDB(GET_INPUT_ID, txId, connection);
-				for (int j = 1; inputs.next(); j++) {
-					logger.trace("Removing inputs");
-					long inputId = result.getLong(j);
+				while(inputs.next()){
+					long inputId = result.getLong("input_id");
 					removeFromDB(REMOVE_INPUT_SCRIPT, inputId, connection);
 				}
 				inputs.close();
 
 				//remove outputs and inputs
 				removeFromDB(REMOVE_OUTPUT, txId, connection);
-				removeFromDB(REMOVE_INPUT, txId, connection);
+				removeFromDB(REMOVE_INPUT, txId , connection);
 
 				//set outputs to unspent
 				PreparedStatement unspentstatement = connection.getPreparedStatement(MARK_AS_UNSPENT);
@@ -100,63 +92,53 @@ public class BlockDeleter {
 
 				//remove transaction
 				removeFromDB(REMOVE_TRANSACTION, txId, connection);
-				logger.debug("Removing transaction #" + txId);
 			}
-
-			logger.info("Transactions removed");
 
 			//remove block
 			removeFromDB(REMOVE_BLOCK, blkId, connection);
 			connection.commit();
 
 		} catch (SQLException e) {
-			logger.fatal("Something went wrong while trying to remove block " + blockHash);
-			logger.fatal("failed at", e);
-			System.exit(1);
-		} finally {
+			e.printStackTrace();
+		}finally{
 			closeStatements();
 		}
 
-		logger.info("removed block from datbase: " + blockHash);
+		logger.info("removed block from datbase: "+blockHash );
 
 	}
 
-	private void closeStatements() {
+	private void closeStatements(){
 		try {
-			for (PreparedStatement statement : statements)
+			for(PreparedStatement statement: statements){
 				statement.close();
-			statements = new ArrayList<>();
+				statements.remove(statement);
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
+		}	
 	}
 
-	private ResultSet getFromDB(String sql, long id, DatabaseConnection connection) {
+	private ResultSet getFromDB(String sql,long id, DatabaseConnection connection){
 
-		PreparedStatement statement = connection.getPreparedStatement(sql);
+		PreparedStatement statement = connection.getPreparedStatement(sql);	
 		statements.add(statement);
-		ResultSet result = null;
+		ResultSet result=null;
 		try {
 			statement.setLong(1, id);
 			result = statement.executeQuery();
-			logger.debug(result.toString());
-			//statement.closeOnCompletion();
 		} catch (SQLException e) {
-			logger.fatal("Error while talking to the database");
-			logger.fatal("failed at", e);
-			connection.commit();
-			connection.closeConnection();
-			System.exit(1);
+			e.printStackTrace();
 		}
 		return result;
 	}
 
-	private void removeFromDB(String sql, long id, DatabaseConnection connection) {
-		PreparedStatement statement = connection.getPreparedStatement(sql);
+	private void removeFromDB(String sql,long id,DatabaseConnection connection){
+		PreparedStatement statement = connection.getPreparedStatement(sql);	
 		statements.add(statement);
 		try {
 			statement.setLong(1, id);
-			statement.executeUpdate();
+			statement.executeUpdate();	
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
