1a89301309a5c17f6fe4fee7c59bf2b878c51782
Niklaus Hofer <niklaus@mykolab.ch>
2016-04-14 16:24:33 +0200

    More queries/stored procedures

---
+++ src/resources/sql/count_addr_higher_received.sql
@@ -0,0 +1,19 @@
+-- Count how many addresses currently have a balance higher than the given parameter
+DELIMITER // 
+
+CREATE OR REPLACE PROCEDURE count_addr_which_received_higher_than( myamount bigint )
+  BEGIN
+    SELECT COUNT(address) AS count
+    FROM
+      (
+        SELECT address,SUM(amount) AS sum_amount
+        FROM output
+        GROUP BY address
+      ) AS sum_addr
+      WHERE sum_amount > myamount;
+  END;
+
+//
+DELIMITER ;
+
+
