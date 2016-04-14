6df2790ed90215bd7461c1a3c11253a9c49408f0
Niklaus Hofer <niklaus@mykolab.ch>
2016-04-14 12:43:25 +0200

    Two more stored procedures

---
+++ src/resources/sql/balance_at_time.sql
@@ -0,0 +1,19 @@
+-- Get the balance of an address at a given time
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE get_balance( addr varchar(255), time DATETIME )
+  BEGIN
+    SELECT SUM(amount) as balance
+      FROM output
+      LEFT JOIN transaction
+        ON output.tx_id = transaction.tx_id
+      WHERE transaction.blk_time < time
+        AND (
+          output.spent_at is NULL
+          OR output.spent_at > time
+        )  
+        AND address = addr;
+  END;
+
+//
+DELIMITER ;
