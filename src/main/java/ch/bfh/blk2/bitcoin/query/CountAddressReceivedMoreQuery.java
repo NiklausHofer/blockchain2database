7bceb94aa3343e5b287b7b221bf2797e07af6ec3
Niklaus Hofer <niklaus@mykolab.ch>
2016-04-14 14:13:34 +0200

    Migrated another query to a stored procedure

--- src/main/java/ch/bfh/blk2/bitcoin/query/CountAddressReceivedMoreQuery.java
+++ src/main/java/ch/bfh/blk2/bitcoin/query/CountAddressReceivedMoreQuery.java
@@ -3,50 +3,44 @@ package ch.bfh.blk2.bitcoin.query;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import ch.bfh.blk2.bitcoin.blockchain2database.DatabaseConnection;
 
-public class CountAddressReceivedMoreQuery implements Query<Long>{
+public class CountAddressReceivedMoreQuery implements Query<Long> {
 
 	private static final Logger logger = LogManager.getLogger("CountAddressReceivedMoreQuery");
-	
-	private static final String SQL=
-			"SELECT COUNT(addr_id) AS count FROM"
-			+ " (SELECT addr_id,SUM(amount) AS sum_amount"
-			+ " FROM output "
-			+ " GROUP BY addr_id) AS sum_addr"
-			+ " WHERE sum_amount > ?";
-	
+
+	private static final String SQL = "call count_addr_which_received_higher_than(?)";
+
 	private long amount;
-	private long result=-1;
-	
+	private long result = -1;
+
 	/**
 	 * Get the number of addresses that received more than a given amount
-	 * 
+	 *
 	 */
-	public CountAddressReceivedMoreQuery(long amount) {		
-		this.amount= amount;
+	public CountAddressReceivedMoreQuery(long amount) {
+		this.amount = amount;
 	}
-	
+
 	@Override
 	public void exequte(DatabaseConnection connection) {
-		
-		logger.info("Counting Addresses that received more than"+this.amount);
-		
-		try{
-		PreparedStatement statement = connection.getPreparedStatement(SQL);
-		statement.setLong(1, this.amount);
-		
-		ResultSet resultSet = statement.executeQuery();
-		
-		if(resultSet.next())
-			this.result = resultSet.getLong("count");
-					
-		} catch (SQLException e) {	
+
+		logger.info("Counting Addresses that received more than" + this.amount);
+
+		try {
+			PreparedStatement statement = connection.getPreparedStatement(SQL);
+			statement.setLong(1, this.amount);
+
+			ResultSet resultSet = statement.executeQuery();
+
+			if (resultSet.next())
+				this.result = resultSet.getLong("count");
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 			logger.fatal("Failed to exequte Query");
 			connection.commit();
@@ -60,15 +54,13 @@ public class CountAddressReceivedMoreQuery implements Query<Long>{
 		return this.result;
 	}
 
-	public String resultToString(){
-		
-		if(this.result<1)
-			return "no address received mor than "+ this.amount;
-		else{
-			return "found "+this.result+" addresses that received more than "
-					+this.amount+" satoshi";
-		}
-	}
+	@Override
+	public String resultToString() {
 
+		if (this.result < 1)
+			return "no address received mor than " + this.amount;
+		else
+			return "found " + this.result + " addresses that received more than " + this.amount + " satoshi";
+	}
 
 }
