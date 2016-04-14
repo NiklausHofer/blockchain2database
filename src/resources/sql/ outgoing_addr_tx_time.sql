11ef563c9821ced76429e113d2c1b7e0582a2403
Stefan <andos1@bfh.ch>
2016-04-14 12:37:39 +0200

    sql script to create stored procedures

---
+++ src/resources/sql/ outgoing_addr_tx_time.sql
@@ -0,0 +1,18 @@
+-- OUTGOING TRANSACTIONS OF AN ADDRESS AT A GIVEN TIME
+
+DELIMITER //
+CREATE PROCEDURE outgoing_addr_tx_time
+(IN addr VARCHAR(35), dat DATETIME) 
+BEGIN
+  SELECT transaction.tx_id, transaction.tx_hash
+  FROM block,transaction INNER JOIN (
+        input INNER JOIN output 
+        ON input.prev_tx_id = output.tx_id 
+        AND input.prev_output_index = output.tx_index)
+    ON input.tx_id = transaction.tx_id
+  WHERE output.address = addr
+  AND block.blk_id = transaction.blk_id
+  AND block.time < dat;
+END;
+//
+DELIMITER ;
