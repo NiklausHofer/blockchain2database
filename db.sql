8d2d10102338f9c7c7f6ae10e8af51b5f243a34e
Niklaus Hofer <niklaus@mykolab.ch>
2016-04-27 12:31:38 +0200

    Some cleanup work

--- db.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER IF NOT EXISTS 'mainnet'@'localhost' IDENTIFIED BY 'foobar';
-DROP DATABASE IF EXISTS btc;
-CREATE DATABASE btc;
-GRANT ALL PRIVILEGES ON btc.* TO 'mainnet'@'localhost';
-
