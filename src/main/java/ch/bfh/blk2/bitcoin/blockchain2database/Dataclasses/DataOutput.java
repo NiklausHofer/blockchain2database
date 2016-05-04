731078ed199de665f6ac285fcf274948fe8c75dd
Niklaus Hofer <niklaus@mykolab.ch>
2016-05-04 15:50:13 +0200

    Cleaned the DataOutput up to fit our new design

--- src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/Dataclasses/DataOutput.java
+++ src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/Dataclasses/DataOutput.java
@@ -5,32 +5,19 @@ import java.sql.SQLException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bitcoinj.core.ScriptException;
 import org.bitcoinj.core.TransactionOutput;
 
 import ch.bfh.blk2.bitcoin.blockchain2database.DatabaseConnection;
-import ch.bfh.blk2.bitcoin.util.PropertiesLoader;
-import ch.bfh.blk2.bitcoin.util.Utility;
 
 public class DataOutput {
 
 	private static final Logger logger = LogManager.getLogger("DataOutput");
 
 	private static final String INSERT_OUTPUT = "INSERT INTO output"
-			+ " (amount, tx_id, tx_index, scriptType)"
-			+ " VALUES(?, ?, ?, ?);",
-
-	INSERT_SMALL_SCRIPT = "INSERT IGNORE INTO small_out_script (tx_id,tx_index,script_size,script, isOpReturn, isPayToScriptHash, isSentToAddress, isSentoToMultiSig, isSentToRawPubKey)"
-			+ " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);",
-
-	INSERT_LARGE_SCRIPT = "INSERT IGNORE INTO large_out_script (tx_id,tx_index,script_size,script, isOpReturn, isPayToScriptHash, isSentToAddress, isSentoToMultiSig, isSentToRawPubKey)"
-			+ " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);";
-
-	private static int maxScriptSize = Integer
-			.parseInt(PropertiesLoader.getInstance().getProperty("max_inmemory_output_script"));
+			+ " (tx_id, tx_index, amount, scriptType)"
+			+ " VALUES(?, ?, ?, ?);";
 
 	private TransactionOutput output;
-	private byte[] script = null;
 	private long txId;
 
 	public DataOutput(TransactionOutput output, long txId) {
@@ -40,38 +27,17 @@ public class DataOutput {
 
 	public void writeOutput(DatabaseConnection connection) {
 
-		String address = null;
-
-		OutputScript outScript;
-
 		try {
-			address = Utility.getAddressFromOutput(output).toString();
-		} catch (ScriptException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println("Invalid Address: error" + e.getClass());
-		}
+			PreparedStatement statement = connection.getPreparedStatement(INSERT_OUTPUT);
 
-		try {
+			statement.setLong(1, txId);
+			statement.setLong(2, output.getIndex());
+			statement.setLong(3, output.getValue().getValue());
 
-			PreparedStatement statement = (PreparedStatement) connection.getPreparedStatement(INSERT_OUTPUT);
-
-			statement.setLong(1, output.getValue().getValue());
-			statement.setLong(2, txId);
-			statement.setLong(3, output.getIndex());
-
-			if (address == null)
-				statement.setNull(4, java.sql.Types.NULL);
-			else
-				statement.setString(4, address);
-
-			outScript = OuputScriptCreator.parseScript(output,txId,output.getIndex());
-
-			statement.setInt(5, outScript.getType().getValue());
+			OutputScript outScript = OuputScriptCreator.parseScript(output, txId, output.getIndex());
+			statement.setInt(4, outScript.getType().getValue());
 
 			statement.executeUpdate();
-
 			statement.close();
 
 			outScript.writeOutputScript(connection);
