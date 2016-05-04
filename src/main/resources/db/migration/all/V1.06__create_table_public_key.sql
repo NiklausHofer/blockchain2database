173cefe1a390255f02dcffb278df8f9b8f9341f1
Niklaus Hofer <niklaus@mykolab.ch>
2016-05-04 13:40:00 +0200

    Grossen einiger Felder in der DB angepasst

--- src/main/resources/db/migration/all/V1.06__create_table_public_key.sql
+++ src/main/resources/db/migration/all/V1.06__create_table_public_key.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS public_key(
   id BIGINT,
   pubkey_hash VARCHAR(64) UNIQUE,
-  pubkey VARBINARY(512) UNIQUE,
+  pubkey VARBINARY(520) UNIQUE,
     PRIMARY KEY(id)
 )ENGINE = MEMORY;
 
--- src/main/resources/db/migration/all/V1.14__create_table_script.sql
+++ src/main/resources/db/migration/all/V1.14__create_table_script.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS script(
   script_id BIGINT,
   script_index INT,
   op_code INT,
-  data VARCHAR(32),
+  data VARBINARY(76),
     PRIMARY KEY(script_id, script_index),
     FOREIGN KEY(op_code) REFERENCES op_codes(op_code)
-)ENGINE = MEMORY;
+)ENGINE = InnoDB;
