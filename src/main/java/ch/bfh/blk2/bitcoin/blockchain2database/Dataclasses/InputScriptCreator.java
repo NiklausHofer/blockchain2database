076a17d5838d8f9c710adde5e54aec85eb64e39f
Niklaus Hofer <niklaus@mykolab.ch>
2016-05-05 13:08:34 +0200

    Multisig input script saved to database

--- src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/Dataclasses/InputScriptCreator.java
+++ src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/Dataclasses/InputScriptCreator.java
@@ -1,7 +1,5 @@
 package ch.bfh.blk2.bitcoin.blockchain2database.Dataclasses;
 
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bitcoinj.core.ScriptException;
@@ -10,66 +8,54 @@ import org.bitcoinj.script.Script;
 import org.bitcoinj.script.ScriptChunk;
 
 public class InputScriptCreator {
-		
+
 	private static final Logger logger = LogManager.getLogger("InputScriptCreator");
 
-	public static InputScript parseScript
-		(TransactionInput in, long txId, int txIndex,ScriptType prefOutType, long prevTxId, int prevTxIndex){
-		
-		byte [] inputBytes = in.getScriptBytes();
+	public static InputScript parseScript(TransactionInput in, long txId, int txIndex, ScriptType prefOutType,
+			long prevTxId, int prevTxIndex) {
+
+		byte[] inputBytes = in.getScriptBytes();
 		int scriptSize = inputBytes.length;
 		Script script = new Script(inputBytes);
-		
-		if(prefOutType == ScriptType.OUT_MULTISIG){
-			return new MultisigInputScript();
-		}
-		if(prefOutType == ScriptType.OUT_P2PKHASH){
+
+		if (prefOutType == ScriptType.OUT_MULTISIG)
+			return new MultisigInputScript(txId, txIndex, script, scriptSize);
+		if (prefOutType == ScriptType.OUT_P2PKHASH)
 			return new P2PKHInputScript();
-		}
-		if(prefOutType == ScriptType.OUT_P2RAWPUBKEY){
+		if (prefOutType == ScriptType.OUT_P2RAWPUBKEY)
 			return new P2RawPubKeyInputscript();
-		}
-		if(prefOutType == ScriptType.OUT_P2SH){
-		
-			if(isP2SHMultisig(script)){
+		if (prefOutType == ScriptType.OUT_P2SH)
+			if (isP2SHMultisig(script))
 				return new P2SHMultisigInputScript();
-			}else{
+			else
 				return new P2SHOtherInputScript();
-			}
-		}
-		if(prefOutType == ScriptType.OUT_OTHER){
+		if (prefOutType == ScriptType.OUT_OTHER)
 			return new OtherInputScript();
-		}
-		if(prefOutType == ScriptType.NO_PREV_OUT){
+		if (prefOutType == ScriptType.NO_PREV_OUT)
 			return new CoinbaseInputScript();
-		}
-		
+
 		// input script must be one of these types
 		// input script can't be invalid
-		
+
 		logger.fatal("Infalid Inputscript");
 		System.exit(1);
 		return null;
 	}
-	
-	
-	private static boolean isP2SHMultisig(Script script){
-		
-		ScriptChunk lastChunk = script.getChunks().get(script.getChunks().size()-1);
-		
-		if(lastChunk.data != null){
-			try{
-			Script reedemScript = new Script(lastChunk.data);	
-			return reedemScript.isSentToMultiSig();
-			}
-			catch(ScriptException e){
+
+	private static boolean isP2SHMultisig(Script script) {
+
+		ScriptChunk lastChunk = script.getChunks().get(script.getChunks().size() - 1);
+
+		if (lastChunk.data != null)
+			try {
+				Script reedemScript = new Script(lastChunk.data);
+				return reedemScript.isSentToMultiSig();
+			} catch (ScriptException e) {
 				logger.debug("invalid reedem Script or data");
 				logger.debug("cant parse to script");
 				return false;
 			}
-			
-		}else{
+		else
 			return false;
-		}
 	}
 }
--- src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/Dataclasses/MultisigInputScript.java
+++ src/main/java/ch/bfh/blk2/bitcoin/blockchain2database/Dataclasses/MultisigInputScript.java
@@ -1,5 +1,95 @@
 package ch.bfh.blk2.bitcoin.blockchain2database.Dataclasses;
 
-public class MultisigInputScript implements InputScript{
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptChunk;
+
+import ch.bfh.blk2.bitcoin.blockchain2database.DatabaseConnection;
+
+public class MultisigInputScript implements InputScript {
+
+	private static final Logger logger = LogManager.getLogger("MultisigInputScript");
+
+	// Not doing this in this version
+	//private final String GET_PUBKEYS = "SELECT public_key.pubkey FROM multisig_pubkeys LEFT JOIN public_key ON multisig_pubkeys.public_key_id = public_key.id WHERE multisig_pubkeys.tx_id = ? AND multisig_pubkeys.tx_index = ?;";
+	private final String INSERT_QUERY = "INSERT INTO unlock_script_multisig(tx_id, tx_index, script_size) VALUES( ?, ?, ? );";
+	private final String CONNECTION_QUERY = "INSERT INTO multisig_signature(tx_id, tx_index, signature_id, index) VALUES( ?, ?, ?, ? );";
+
+	private long tx_id;
+	private int tx_index;
+	private Script script;
+	private int script_size;
+
+	public MultisigInputScript(long tx_id, int tx_index, Script script, int script_size) {
+		this.tx_id = tx_id;
+		this.tx_index = tx_index;
+		this.script = script;
+		this.script_size = script_size;
+	}
+
+	@Override
+	public ScriptType getType() {
+		return ScriptType.IN_MULTISIG;
+	}
+
+	@Override
+	public void writeInput(DatabaseConnection connection) {
+		PreparedStatement insertStatement = connection.getPreparedStatement(INSERT_QUERY);
+
+		try {
+			insertStatement.setLong(1, tx_id);
+			insertStatement.setInt(2, tx_index);
+			insertStatement.setInt(3, script_size);
+
+			insertStatement.executeUpdate();
+
+			insertStatement.close();
+		} catch (SQLException e) {
+			logger.fatal("Unable to write input script for input #" + tx_index + " of transaction with id " + tx_id, e);
+			System.exit(1);
+		}
+
+		SigManager sima = new SigManager();
+
+		int index = 0;
+		for (ScriptChunk chunk : script.getChunks()) {
+			if (chunk.opcode > 75) {
+				logger.warn("The script of input #"
+						+ tx_index
+						+ " of transaction w/ id "
+						+ tx_id
+						+ " is supposed to be of type multisig. But it looks like this:");
+				logger.warn(script.toString());
+			}
+			long sigId = sima.saveAndGetSigId(connection, chunk.data);
+
+			PreparedStatement connectionStatement = connection.getPreparedStatement(CONNECTION_QUERY);
+
+			try {
+				connectionStatement.setLong(1, tx_id);
+				connectionStatement.setInt(2, tx_index);
+				connectionStatement.setLong(3, sigId);
+				connectionStatement.setInt(4, index);
+
+				connectionStatement.executeUpdate();
+
+				connectionStatement.close();
+			} catch (SQLException e) {
+				logger.fatal("Unable to connect input #"
+						+ tx_index
+						+ " of transaction "
+						+ tx_id
+						+ " with signature "
+						+ sigId, e);
+				System.exit(1);
+			}
+			index++;
+		}
+
+	}
 
 }
