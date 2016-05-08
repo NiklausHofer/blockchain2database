d9ab4a60b976d465f164170e270a82013b1e535f
Niklaus Hofer <niklaus@mykolab.ch>
2016-05-08 17:05:48 +0200

    reenable foregin key from script table to op_codes table now that we have the opcodes

--- src/main/resources/db/migration/all/V1.14__create_table_script.sql
+++ src/main/resources/db/migration/all/V1.14__create_table_script.sql
@@ -1,18 +1,18 @@
 -- Use in test only
-CREATE TABLE IF NOT EXISTS script(
-  script_id BIGINT,
-  script_index INT,
-  op_code INT,
-  data VARBINARY(2048),
-    PRIMARY KEY(script_id, script_index)
-)ENGINE = InnoDB;
-
--- for production use
 --CREATE TABLE IF NOT EXISTS script(
 --  script_id BIGINT,
 --  script_index INT,
 --  op_code INT,
---  data VARBINARY(76),
---    PRIMARY KEY(script_id, script_index),
---    FOREIGN KEY(op_code) REFERENCES op_codes(op_code)
+--  data VARBINARY(2048),
+--    PRIMARY KEY(script_id, script_index)
 --)ENGINE = InnoDB;
+
+-- for production use
+CREATE TABLE IF NOT EXISTS script(
+  script_id BIGINT,
+  script_index INT,
+  op_code INT,
+  data VARBINARY(76),
+    PRIMARY KEY(script_id, script_index),
+    FOREIGN KEY(op_code) REFERENCES op_codes(op_code)
+)ENGINE = InnoDB;
