b28a84a2b1b353222211c14edc4d42e6fd781732
Niklaus Hofer <niklaus@mykolab.ch>
2016-06-11 03:30:11 +0200

    Updated database config to simplify MEMORY to InnoDB migration

--- src/main/resources/db/migration/all/V1.98__create_table_parameter.sql
+++ src/main/resources/db/migration/all/V1.98__create_table_parameter.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS parameter(
-   p_key VARCHAR(256),
-   p_value VARCHAR(256) NOT NULL,
+   p_key VARCHAR(64),
+   p_value VARCHAR(64) NOT NULL,
      PRIMARY KEY(p_key)
-) ENGINE = MEMORY;
+) ENGINE = InnoDB;
